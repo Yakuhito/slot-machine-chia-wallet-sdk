cbf8eca225fcab33e8f05dccb7a88325b027d39f
Yak <y@kuhi.to>
2026-01-26 15:13:15 +0900

    first Rue puzzle rewrite (action.clsp) WORKS

--- crates/chia-sdk-types/src/puzzles/action_layer/action_layer_puzzle.rs
+++ crates/chia-sdk-types/src/puzzles/action_layer/action_layer_puzzle.rs
@@ -7,35 +7,35 @@ use hex_literal::hex;
 
 use crate::{MerkleProof, Mod};
 
-pub const ACTION_LAYER_PUZZLE: [u8; 657] = hex!(
+pub const ACTION_LAYER_PUZZLE: [u8; 640] = hex!(
+    // Rue
     "
-    ff02ffff01ff02ff05ffff04ffff04ff0bff1780ffff04ffff02ff0affff04ff
-    02ffff04ff2fffff04ff80ffff04ffff04ffff04ff80ff1780ff8080ffff04ff
-    ff02ff0cffff04ff02ffff04ff0bffff04ff2fffff04ff80ffff04ff5fff8080
-    8080808080ffff04ff81bfff8080808080808080ff81ff808080ffff04ffff01
-    ffffff02ffff03ffff09ff05ff1380ffff01ff0101ffff01ff02ff08ffff04ff
-    02ffff04ff05ffff04ff1bff808080808080ff0180ff02ffff03ff2fffff01ff
-    02ffff03ffff02ffff03ff81cfffff01ff09ff05ffff02ff1effff04ff02ffff
-    04ffff0bffff0101ffff02ff16ffff04ff02ffff04ffff02ff818fff0b80ff80
-    80808080ffff04ff81cfff808080808080ffff01ff02ff08ffff04ff02ffff04
-    ff818fffff04ff17ff808080808080ff0180ffff01ff02ff0cffff04ff02ffff
-    04ff05ffff04ff0bffff04ffff04ff818fff1780ffff04ff6fff808080808080
-    80ffff01ff088080ff0180ffff011780ff0180ffff02ffff03ff2fffff01ff02
-    ff0affff04ff02ffff04ff05ffff04ffff04ff37ff0b80ffff04ffff02ffff02
-    ff4fff0580ffff04ff27ff819f8080ffff04ff6fffff04ff81dfff8080808080
-    808080ffff01ff04ff27ffff04ff37ff0b808080ff0180ffff02ffff03ffff07
-    ff0580ffff01ff0bffff0102ffff02ff16ffff04ff02ffff04ff09ff80808080
-    ffff02ff16ffff04ff02ffff04ff0dff8080808080ffff01ff0bffff0101ff05
-    8080ff0180ff02ffff03ff1bffff01ff02ff1effff04ff02ffff04ffff02ffff
-    03ffff18ffff0101ff1380ffff01ff0bffff0102ff2bff0580ffff01ff0bffff
-    0102ff05ff2b8080ff0180ffff04ffff04ffff17ff13ffff0181ff80ff3b80ff
-    8080808080ffff010580ff0180ff018080
+    ff02ffff01ff02ff05ffff04ffff04ff0bff1780ffff04ffff02ff1effff04ff
+    1effff04ffff04ffff02ff0cffff04ffff04ffff04ff08ff0c80ffff04ff0aff
+    168080ffff04ffff04ff80ff5f80ffff04ff0bff2f80808080ff2f80ffff04ff
+    80ffff04ffff04ffff04ff80ff1780ff8080ff81bf8080808080ff81ff808080
+    ffff04ffff04ffff04ffff01ff02ffff03ffff07ff0380ffff01ff0bffff0102
+    ffff02ff02ffff04ff02ff058080ffff02ff02ffff04ff02ff07808080ffff01
+    ff0bffff0101ff038080ff0180ffff01ff02ffff03ff0dffff01ff02ffff03ff
+    ff02ffff03ff35ffff01ff09ff0bffff02ff0affff04ff0affff04ff35ffff0b
+    ffff0101ffff02ff08ffff04ff08ffff02ff25ff0f8080808080808080ffff01
+    ff02ff0effff04ff0effff04ff25ff0980808080ff0180ffff01ff02ff0cffff
+    04ffff04ffff04ff08ff0c80ffff04ff0aff0e8080ffff04ffff04ffff04ff25
+    ff0980ff1d80ffff04ff0bff0f80808080ffff01ff088080ff0180ffff010980
+    ff018080ffff04ffff01ff02ffff03ff0dffff01ff02ff02ffff04ff02ffff04
+    ffff04ffff17ff09ffff0181ff80ff1d80ffff0bffff0102ffff03ffff18ff09
+    ffff010180ff15ff0780ffff03ffff18ff09ffff010180ff07ff158080808080
+    ffff010780ff0180ffff04ffff01ff02ffff03ffff09ff0bff0580ffff01ff01
+    01ffff01ff02ff02ffff04ff02ffff04ff05ff0f80808080ff0180ffff01ff02
+    ffff03ff09ffff01ff02ff02ffff04ff02ffff04ffff04ff19ff0d80ffff04ff
+    ff04ff37ff0b80ffff04ffff02ffff02ff11ff0d80ffff04ff27ff2f8080ff3f
+    8080808080ffff01ff04ff27ffff04ff37ff0b808080ff0180808080ff018080
     "
 );
 
 pub const ACTION_LAYER_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
     "
-    d07619284300be4855f57b76789932021f60e217e000edfee07f9ca7e4b3f49a
+    ddce67597b2f47fa9a005527e19b637f7779da2f195e1a89d9907670863e4fa7
     "
 ));
 
