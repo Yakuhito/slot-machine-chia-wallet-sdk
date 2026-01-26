c62f9e54c49dc4c300d1790622ac63f76e3b880c
Yak <y@kuhi.to>
2026-01-26 15:38:56 +0900

    damn, new finalizer actually works

--- crates/chia-sdk-types/src/puzzles/action_layer/default_finalizer.rs
+++ crates/chia-sdk-types/src/puzzles/action_layer/default_finalizer.rs
@@ -7,34 +7,30 @@ use hex_literal::hex;
 
 use crate::{puzzles::ACTION_LAYER_PUZZLE_HASH, Mod};
 
-pub const DEFAULT_FINALIZER_PUZZLE: [u8; 616] = hex!(
+pub const DEFAULT_FINALIZER_PUZZLE: [u8; 494] = hex!(
     "
-    ff02ffff01ff04ffff04ff10ffff04ffff02ff12ffff04ff02ffff04ff05ffff
-    04ffff02ff12ffff04ff02ffff04ff17ffff04ffff0bffff0101ff1780ff8080
-    808080ffff04ffff0bffff0101ff4f80ffff04ffff02ff1effff04ff02ffff04
-    ff82019fff80808080ff80808080808080ffff04ffff0101ffff04ffff04ff0b
-    ff8080ff8080808080ffff02ff1affff04ff02ffff04ff81dfff8080808080ff
-    ff04ffff01ffffff3302ffff02ffff03ff05ffff01ff0bff7cffff02ff16ffff
-    04ff02ffff04ff09ffff04ffff02ff14ffff04ff02ffff04ff0dff80808080ff
-    808080808080ffff016c80ff0180ffffa04bf5122f344554c53bde2ebb8cd2b7
-    e3d1600ad631c385a5d7cce23c7785459aa09dcf97a184f32623d11a73124ceb
-    99a5709b083721e878a16d78f596718ba7b2ffa102a12871fee210fb8619291e
-    aea194581cbd2531e4b23759d225f6806923f63222a102a8d5dd63fba471ebcb
-    1f3e8f7c1e1879b7152a6e7298a91ce119a63400ade7c5ffffff0bff5cffff02
-    ff16ffff04ff02ffff04ff05ffff04ffff02ff14ffff04ff02ffff04ff07ff80
-    808080ff808080808080ff02ffff03ff09ffff01ff04ff11ffff02ff1affff04
-    ff02ffff04ffff04ff19ff0d80ff8080808080ffff01ff02ffff03ff0dffff01
-    ff02ff1affff04ff02ffff04ff0dff80808080ff8080ff018080ff0180ffff0b
-    ff18ffff0bff18ff6cff0580ffff0bff18ff0bff4c8080ff02ffff03ffff07ff
-    0580ffff01ff0bffff0102ffff02ff1effff04ff02ffff04ff09ff80808080ff
-    ff02ff1effff04ff02ffff04ff0dff8080808080ffff01ff0bffff0101ff0580
-    80ff0180ff018080
+    ff02ffff01ff04ffff04ffff0133ffff04ffff02ff0affff04ff0effff04ff05
+    ffff04ffff02ff0affff04ff0effff04ff17ffff04ffff0bffff0101ff1780ff
+    8080808080ffff04ffff0bffff0101ff4f80ffff04ffff02ff0cffff04ff0cff
+    82019f8080ff80808080808080ffff04ffff0101ffff04ffff04ff0bff8080ff
+    8080808080ffff02ff08ffff04ff08ff81df808080ffff04ffff04ffff04ffff
+    01ff02ffff03ff05ffff01ff04ff09ffff02ff02ffff04ff02ffff04ff0dff07
+    80808080ffff01ff02ffff03ff07ffff01ff02ff02ffff04ff02ff078080ffff
+    018080ff018080ff0180ffff01ff02ffff03ffff07ff0380ffff01ff0bffff01
+    02ffff02ff02ffff04ff02ff058080ffff02ff02ffff04ff02ff07808080ffff
+    01ff0bffff0101ff038080ff018080ffff04ffff01ff0bffff0102ffff0bffff
+    0182010280ffff0bffff0102ffff0bffff0102ffff0bffff0182010180ff0580
+    ffff0bffff0102ffff02ff02ffff04ff02ff078080ffff0bffff010180808080
+    ffff01ff02ffff03ff03ffff01ff0bffff0102ffff0bffff0182010480ffff0b
+    ffff0102ffff0bffff0102ffff0bffff0182010180ff0580ffff0bffff0102ff
+    ff02ff02ffff04ff02ff078080ffff0bffff010180808080ffff01ff0bffff01
+    8201018080ff01808080ff018080
     "
 );
 
 pub const DEFAULT_FINALIZER_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
     "
-    5ec944c72fd72b55f7c753f4ded0f3fe7387958f2ebbb35ee3c40f062498b93f
+    9d274de59aeaa128d1b0a802b52911838420ef9720d605c78942e8dadfc0810b
     "
 ));
 
