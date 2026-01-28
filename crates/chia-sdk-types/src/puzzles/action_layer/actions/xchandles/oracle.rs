8051f268eb57431ee4721c9f3fa47415bcc8bf10
Yak <y@kuhi.to>
2026-01-29 00:02:52 +0900

    oracle worked

--- crates/chia-sdk-types/src/puzzles/action_layer/actions/xchandles/oracle.rs
+++ crates/chia-sdk-types/src/puzzles/action_layer/actions/xchandles/oracle.rs
@@ -7,32 +7,31 @@ use hex_literal::hex;
 
 use crate::Mod;
 
-pub const XCHANDLES_ORACLE_PUZZLE: [u8; 571] = hex!(
+pub const XCHANDLES_ORACLE_PUZZLE: [u8; 499] = hex!(
+    // Rue
     "
-    ff02ffff01ff04ff0bffff02ff16ffff04ff02ffff04ff05ffff04ffff02ff2e
-    ffff04ff02ffff04ff0fff80808080ff808080808080ffff04ffff01ffffff33
-    3eff4202ffffffffa04bf5122f344554c53bde2ebb8cd2b7e3d1600ad631c385
-    a5d7cce23c7785459aa09dcf97a184f32623d11a73124ceb99a5709b083721e8
-    78a16d78f596718ba7b2ffa102a12871fee210fb8619291eaea194581cbd2531
-    e4b23759d225f6806923f63222a102a8d5dd63fba471ebcb1f3e8f7c1e1879b7
-    152a6e7298a91ce119a63400ade7c5ff04ff10ffff04ffff0bff52ffff0bff1c
-    ffff0bff1cff62ff0580ffff0bff1cffff0bff72ffff0bff1cffff0bff1cff62
-    ffff0bffff0101ff0b8080ffff0bff1cff62ff42808080ff42808080ffff04ff
-    80ffff04ffff04ff05ff8080ff8080808080ffff04ffff02ff3effff04ff02ff
-    ff04ff05ffff04ff0bff8080808080ffff04ffff02ff1affff04ff02ffff04ff
-    05ffff04ff0bff8080808080ffff04ffff04ff18ffff04ffff0effff016fff0b
-    80ff808080ff80808080ffff02ffff03ffff07ff0580ffff01ff0bffff0102ff
-    ff02ff2effff04ff02ffff04ff09ff80808080ffff02ff2effff04ff02ffff04
-    ff0dff8080808080ffff01ff0bffff0101ff058080ff0180ff04ff14ffff04ff
-    ff0112ffff04ff80ffff04ffff0bff52ffff0bff1cffff0bff1cff62ff0580ff
-    ff0bff1cffff0bff72ffff0bff1cffff0bff1cff62ffff0bffff0101ff0b8080
-    ffff0bff1cff62ff42808080ff42808080ff8080808080ff018080
+    ff02ffff01ff02ffff01ff04ff17ffff04ffff04ffff0142ffff04ffff0112ff
+    ff04ff80ffff04ffff02ff15ffff04ff2dffff04ff0bffff04ffff0bffff0101
+    ff0280ff8080808080ff8080808080ffff04ffff02ff3dffff04ffff04ff15ff
+    2d80ffff04ff0bff02808080ffff04ffff04ffff013effff04ffff0effff016f
+    ff0280ff808080ff8080808080ffff04ffff02ff04ffff04ff04ff0f8080ff01
+    8080ffff04ffff04ffff01ff02ffff03ffff07ff0380ffff01ff0bffff0102ff
+    ff02ff02ffff04ff02ff058080ffff02ff02ffff04ff02ff07808080ffff01ff
+    0bffff0101ff038080ff0180ffff04ffff01ff0bffff0102ffff0bffff018201
+    0280ffff0bffff0102ffff0bffff0102ffff0bffff0182010180ff0580ffff0b
+    ffff0102ffff02ff02ffff04ff02ff078080ffff0bffff010180808080ffff04
+    ffff01ff02ffff03ff03ffff01ff0bffff0102ffff0bffff0182010480ffff0b
+    ffff0102ffff0bffff0102ffff0bffff0182010180ff0580ffff0bffff0102ff
+    ff02ff02ffff04ff02ff078080ffff0bffff010180808080ffff01ff0bffff01
+    8201018080ff0180ffff01ff04ffff0133ffff04ffff02ff04ffff04ff06ffff
+    04ff05ffff04ffff0bffff0101ff0780ff8080808080ffff04ff80ffff04ffff
+    04ff05ff8080ff8080808080808080ff018080
     "
 );
 
 pub const XCHANDLES_ORACLE_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
     "
-    ccf9ba923d04ba90a194ecb1a26f59912c838a4d6b175a4f041c59127cbe38e3
+    2647db165fc60f83c7afb267fb7ec780302f7fa2138ad54140e50bfdb8a19388
     "
 ));
 
