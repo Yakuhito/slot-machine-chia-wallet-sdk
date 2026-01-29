44149fcea46057d7c72f0428fe2ad1eb49106ff3
Yak <y@kuhi.to>
2026-01-29 21:57:29 +0900

    sync puzzle

--- crates/chia-sdk-types/src/puzzles/action_layer/actions/reward_distributor/sync.rs
+++ crates/chia-sdk-types/src/puzzles/action_layer/actions/reward_distributor/sync.rs
@@ -6,24 +6,24 @@ use hex_literal::hex;
 
 use crate::Mod;
 
-pub const REWARD_DISTRIBUTOR_SYNC_PUZZLE: [u8; 295] = hex!(
+pub const REWARD_DISTRIBUTOR_SYNC_PUZZLE: [u8; 257] = hex!(
+    // Rue
     "
-    ff02ffff01ff02ffff03ffff22ffff20ffff15ff0bff81fd8080ffff15ff0bff
-    81bd8080ffff01ff04ffff04ff09ffff04ff15ffff04ff2dffff04ffff02ff0e
-    ffff04ff02ffff04ff2dffff04ff819dffff04ff81ddffff04ffff02ffff03ff
-    ff15ff2dff8080ffff01ff05ffff14ffff12ff81ddffff11ff0bff81bd8080ff
-    ff12ff2dffff11ff81fdff81bd80808080ff8080ff0180ff80808080808080ff
-    ff04ff0bff81fd8080808080ffff04ffff04ff04ffff04ff0bff808080ffff04
-    ffff04ff0affff04ffff0effff0173ffff0bffff0102ffff0bffff0101ff0b80
-    ffff0bffff0101ff81fd808080ff808080ff80808080ffff01ff088080ff0180
-    ffff04ffff01ff51ff3eff04ffff10ff0bff2f80ffff11ff17ffff12ff2fff05
-    808080ff018080
+    ff02ffff03ffff22ffff21ffff15ff7eff0380ffff09ff03ff7e8080ffff15ff
+    03ff5e8080ffff01ff02ffff01ff04ffff04ff09ffff04ff15ffff04ff2dffff
+    04ffff04ffff10ff819dff0280ffff11ff81ddffff12ff02ff2d808080ffff04
+    ff07ff81fd8080808080ffff04ffff04ffff0151ffff04ff07ff808080ffff04
+    ffff04ffff013effff04ffff0effff0173ffff0bffff0102ffff0bffff0101ff
+    0780ffff0bffff0101ff81fd808080ff808080ff80808080ffff04ffff02ffff
+    03ffff15ff16ff8080ffff01ff13ffff12ff6effff11ff03ff5e8080ffff12ff
+    16ffff11ff7eff5e808080ffff018080ff0180ff018080ffff01ff088080ff01
+    80
     "
 );
 
 pub const REWARD_DISTRIBUTOR_SYNC_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
     "
-    8df5ceda958718d75f83be7f522365c7a6a1c6b8a7147a004faa536e55f2e7b9
+    ae887c11d52e1a53d6b670d224d560c9a18a237ea751ee97be13e2b99edb836e
     "
 ));
 
@@ -37,7 +37,7 @@ impl RewardDistributorSyncActionArgs {
 }
 
 #[derive(FromClvm, ToClvm, Copy, Debug, Clone, PartialEq, Eq)]
-#[clvm(list)]
+#[clvm(transparent)]
 pub struct RewardDistributorSyncActionSolution {
     pub update_time: u64,
 }
