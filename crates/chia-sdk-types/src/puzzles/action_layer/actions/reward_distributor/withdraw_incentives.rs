4ecfb0139c6be13528af9772d57c2ab1ed7c2eeb
Yak <y@kuhi.to>
2026-01-29 22:09:12 +0900

    withdraw incentives Rue puzzle

--- crates/chia-sdk-types/src/puzzles/action_layer/actions/reward_distributor/withdraw_incentives.rs
+++ crates/chia-sdk-types/src/puzzles/action_layer/actions/reward_distributor/withdraw_incentives.rs
@@ -7,40 +7,40 @@ use hex_literal::hex;
 
 use crate::Mod;
 
-pub const REWARD_DISTRIBUTOR_WITHDRAW_INCENTIVES_PUZZLE: [u8; 832] = hex!(
+pub const REWARD_DISTRIBUTOR_WITHDRAW_INCENTIVES_PUZZLE: [u8; 772] = hex!(
+    // Rue
     "
-    ff02ffff01ff04ffff04ff4fffff04ffff11ff81afffff02ffff03ffff09ff81
-    bfffff05ffff14ffff12ff17ff8202ff80ffff01822710808080ffff0181bfff
-    ff01ff088080ff018080ff81ef8080ffff04ffff04ff10ffff04ff5fff808080
-    ffff04ffff02ff3effff04ff02ffff04ff05ffff04ffff02ff2effff04ff02ff
-    ff04ff5fffff04ff8207ffffff04ff8205ffff808080808080ff8080808080ff
-    ff04ffff02ff16ffff04ff02ffff04ff05ffff04ffff02ff2effff04ff02ffff
-    04ff5fffff04ff8207ffffff04ffff11ff8205ffff81bf80ff808080808080ff
-    ff04ffff0bffff0101ff5f80ff808080808080ffff04ffff02ff3effff04ff02
-    ffff04ff0bffff04ffff02ff2effff04ff02ffff04ff5fffff04ff82017fffff
-    04ff8202ffff808080808080ff8080808080ffff04ffff04ff14ffff04ffff01
-    12ffff04ffff0effff0177ffff0bffff0102ffff0bffff0101ff5f80ffff0bff
-    ff0101ff8202ff808080ffff04ff82017fff8080808080ffff04ffff04ffff01
-    81d6ffff04ff18ffff04ff82017fffff04ff81bfffff04ffff04ff82017fff80
-    80ff808080808080ff8080808080808080ffff04ffff01ffffff5533ff4342ff
-    ff02ffffa04bf5122f344554c53bde2ebb8cd2b7e3d1600ad631c385a5d7cce2
-    3c7785459aa09dcf97a184f32623d11a73124ceb99a5709b083721e878a16d78
-    f596718ba7b2ffa102a12871fee210fb8619291eaea194581cbd2531e4b23759
-    d225f6806923f63222a102a8d5dd63fba471ebcb1f3e8f7c1e1879b7152a6e72
-    98a91ce119a63400ade7c5ffff04ff18ffff04ffff0bff5affff0bff12ffff0b
-    ff12ff6aff0580ffff0bff12ffff0bff7affff0bff12ffff0bff12ff6affff0b
-    ffff0101ff0b8080ffff0bff12ff6aff4a808080ff4a808080ffff04ff80ffff
-    04ffff04ff17ff8080ff8080808080ffff0bffff0102ffff0bffff0101ff0580
-    ffff0bffff0102ffff0bffff0101ff0b80ffff0bffff0101ff17808080ff04ff
-    1cffff04ffff0112ffff04ff80ffff04ffff0bff5affff0bff12ffff0bff12ff
-    6aff0580ffff0bff12ffff0bff7affff0bff12ffff0bff12ff6affff0bffff01
-    01ff0b8080ffff0bff12ff6aff4a808080ff4a808080ff8080808080ff018080
+    ff02ffff01ff02ffff03ffff09ff81bfffff13ffff12ff8202ffff1780ffff01
+    8227108080ffff01ff04ffff04ff4fffff04ffff11ff81afff81bf80ff81ef80
+    80ffff04ffff04ffff0155ffff04ff5fff808080ffff04ffff02ff0effff04ff
+    ff04ff04ff0a80ffff04ff05ffff0bffff0102ffff0bffff0101ff5f80ffff0b
+    ffff0102ffff0bffff0101ff8207ff80ffff0bffff0101ff8205ff8080808080
+    80ffff04ffff04ffff0133ffff04ffff02ff04ffff04ff0affff04ff05ffff04
+    ffff0bffff0101ffff0bffff0102ffff0bffff0101ff5f80ffff0bffff0102ff
+    ff0bffff0101ff8207ff80ffff0bffff0101ffff11ff8205ffff81bf80808080
+    80ff8080808080ffff04ff80ffff04ffff04ffff0bffff0101ff5f80ff8080ff
+    8080808080ffff04ffff02ff0effff04ffff04ff04ff0a80ffff04ff0bffff0b
+    ffff0102ffff0bffff0101ff5f80ffff0bffff0102ffff0bffff0101ff82017f
+    80ffff0bffff0101ff8202ff808080808080ffff04ffff04ffff0143ffff04ff
+    ff0112ffff04ffff0effff0177ffff0bffff0102ffff0bffff0101ff5f80ffff
+    0bffff0101ff8202ff808080ffff04ff82017fff8080808080ffff04ffff04ff
+    ff0181d6ffff04ffff0133ffff04ff82017fffff04ff81bfffff04ffff04ff82
+    017fff8080ff808080808080ff8080808080808080ffff01ff088080ff0180ff
+    ff04ffff04ffff01ff0bffff0102ffff0bffff0182010280ffff0bffff0102ff
+    ff0bffff0102ffff0bffff0182010180ff0580ffff0bffff0102ffff02ff02ff
+    ff04ff02ff078080ffff0bffff010180808080ffff04ffff01ff02ffff03ff03
+    ffff01ff0bffff0102ffff0bffff0182010480ffff0bffff0102ffff0bffff01
+    02ffff0bffff0182010180ff0580ffff0bffff0102ffff02ff02ffff04ff02ff
+    078080ffff0bffff010180808080ffff01ff0bffff018201018080ff0180ffff
+    01ff04ffff0142ffff04ffff0112ffff04ff80ffff04ffff02ff04ffff04ff06
+    ffff04ff05ffff04ffff0bffff0101ff0780ff8080808080ff80808080808080
+    ff018080
     "
 );
 
 pub const REWARD_DISTRIBUTOR_WITHDRAW_INCENTIVES_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
     "
-    fe0bf3bdab042a51e848f813098e322be1e394f469032c3efa00a14b96bdfd6e
+    9dd870db7822f3b8e3c76ef938a31bb35c722f90597c5972a30707f6b36de0c5
     "
 ));
 
