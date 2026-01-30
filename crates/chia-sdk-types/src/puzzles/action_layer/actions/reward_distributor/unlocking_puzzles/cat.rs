194e898ea217eb77d37e5b17a0972cc4ef83fb81
Yak <y@kuhi.to>
2026-01-30 22:38:34 +0900

    CAT unlocking puzzle

--- crates/chia-sdk-types/src/puzzles/action_layer/actions/reward_distributor/unlocking_puzzles/cat.rs
+++ crates/chia-sdk-types/src/puzzles/action_layer/actions/reward_distributor/unlocking_puzzles/cat.rs
@@ -7,34 +7,31 @@ use hex_literal::hex;
 
 use crate::{puzzles::NONCE_WRAPPER_PUZZLE_HASH, Mod};
 
-pub const REWARD_DISTRIBUTOR_CAT_UNLOCKING_PUZZLE: [u8; 629] = hex!(
+pub const REWARD_DISTRIBUTOR_CAT_UNLOCKING_PUZZLE: [u8; 485] = hex!(
+    // Rue
     "
     ff02ffff01ff02ffff03ff2fffff01ff0880ffff01ff04ff8202ffffff04ffff
-    04ff14ffff04ffff0117ffff04ffff02ff3effff04ff02ffff04ffff04ffff01
-    01ffff04ffff04ff10ffff04ff5fffff04ff82017fffff04ffff04ff5fff8080
-    ff8080808080ff808080ff80808080ffff04ffff30ff81bfffff02ff05ffff04
-    ffff02ff16ffff04ff02ffff04ff0bffff04ffff02ff3effff04ff02ffff04ff
-    ff04ff5fff8202ff80ff80808080ffff04ff17ff808080808080ff8203ff8080
-    ff82017f80ff8080808080ffff04ffff04ff18ffff04ffff0112ffff04ffff0e
-    ffff0175ff81bf80ffff04ff5fff8080808080ff8080808080ff0180ffff04ff
-    ff01ffffff3343ff4202ffffff02ffff03ff05ffff01ff0bff7affff02ff2eff
-    ff04ff02ffff04ff09ffff04ffff02ff12ffff04ff02ffff04ff0dff80808080
-    ff808080808080ffff016a80ff0180ffffa04bf5122f344554c53bde2ebb8cd2
-    b7e3d1600ad631c385a5d7cce23c7785459aa09dcf97a184f32623d11a73124c
-    eb99a5709b083721e878a16d78f596718ba7b2ffa102a12871fee210fb861929
-    1eaea194581cbd2531e4b23759d225f6806923f63222a102a8d5dd63fba471eb
-    cb1f3e8f7c1e1879b7152a6e7298a91ce119a63400ade7c5ffff0bff5affff02
-    ff2effff04ff02ffff04ff05ffff04ffff02ff12ffff04ff02ffff04ff07ff80
-    808080ff808080808080ffff0bff1cffff0bff1cff6aff0580ffff0bff1cff0b
-    ff4a8080ff02ffff03ffff07ff0580ffff01ff0bffff0102ffff02ff3effff04
-    ff02ffff04ff09ff80808080ffff02ff3effff04ff02ffff04ff0dff80808080
-    80ffff01ff0bffff0101ff058080ff0180ff018080
+    04ffff0142ffff04ffff0117ffff04ffff02ff04ffff04ff04ffff04ffff0101
+    ffff04ffff04ffff0133ffff04ff5fffff04ff82017fffff04ffff04ff5fff80
+    80ff8080808080ff8080808080ffff04ffff30ff81bfffff02ff05ffff04ffff
+    0bffff0102ffff0bffff0182010280ffff0bffff0102ffff0bffff0102ffff0b
+    ffff0182010180ff0b80ffff0bffff0102ffff02ff06ffff04ff06ffff04ffff
+    02ff04ffff04ff04ffff04ff5fff8202ff808080ffff04ff17ff8080808080ff
+    ff0bffff010180808080ff8203ff8080ff82017f80ff8080808080ffff04ffff
+    04ffff0143ffff04ffff0112ffff04ffff0effff0175ff81bf80ffff04ff5fff
+    8080808080ff8080808080ff0180ffff04ffff04ffff01ff02ffff03ffff07ff
+    0380ffff01ff0bffff0102ffff02ff02ffff04ff02ff058080ffff02ff02ffff
+    04ff02ff07808080ffff01ff0bffff0101ff038080ff0180ffff01ff02ffff03
+    ff03ffff01ff0bffff0102ffff0bffff0182010480ffff0bffff0102ffff0bff
+    ff0102ffff0bffff0182010180ff0580ffff0bffff0102ffff02ff02ffff04ff
+    02ff078080ffff0bffff010180808080ffff01ff0bffff018201018080ff0180
+    80ff018080
     "
 );
 
 pub const REWARD_DISTRIBUTOR_CAT_UNLOCKING_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
     "
-    402c5390423408191049f0a7b3f68c268859ec5dcfc3aa7494cca69d1a8d67d0
+    04a695fd5022813b022b0d530bbb7fe35318ebdd421c568155f35f512fb50fa1
     "
 ));
 
